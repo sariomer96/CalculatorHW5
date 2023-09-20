d6dced762fdf8ed6dd4b9787b75476cb67847899
omer <somersari.96@gmail.com>
2023-09-20 09:50:59 +0300

    Initial Commit

---
+++ CalculatorHW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorHW5
+//
+//  Created by Omer on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
